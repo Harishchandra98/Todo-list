cf6ed84923a46ec15b9a11a58d90d1a112859570
SewwandiHarishchndra <sewwandi.harischandra@q4us.dev>
2024-06-01 23:11:47 +0530

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const [row, setRow] = useState<number>(0);
   const [visible, setVisible] = useState<boolean>(false);
   const [newTodo, setNewTodo] = useState<string>("");
+  const [editRow, setEditRow] = useState<number|null>(null)
 
   return <div className='todo-view'>
       <h1>Todo App</h1>
@@ -44,13 +45,50 @@ function App() {
             }}
           >Cancel</button>
         </div>}
-      {todoList.map((item) =>
+      {todoList.map((item, index) =>
+        editRow === index ? <div className='new-todo'>
+        <input 
+          onChange={(e)=>setNewTodo(e.target.value)}
+          defaultValue={item.todo}
+          value={newTodo}
+        />
+        <button
+          onClick={()=>{
+            if (newTodo) {
+              setRow(row+1)
+              setTodoList([...todoList, {id: row, todo:newTodo, checked:false}])
+              setVisible(false)
+              console.log(todoList);
+              setNewTodo("");
+            }
+            else {
+              return <p>Please enter a todo</p>
+            }
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={()=>{
+            setNewTodo("");
+            setVisible(false)
+          }}
+        >Cancel</button>
+      </div>
+        : 
         <div className='todo-list'>
           <input type='checkbox' />
           <p>{item.todo}</p>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>)}
+          <button
+            onClick={() => {setEditRow(index)}}
+          >Edit</button>
+          <button
+            onClick={()=> {
+
+            }}
+          >Delete</button>
+        </div>
+      )}
     </div>
 }
 
